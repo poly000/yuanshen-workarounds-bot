31879102a1bc82d9b206aa12f4a8b027271595c5
poly000 <1348292515@qq.com>
2023-03-10 13:20:47 +0800

    fix invalid header

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,7 @@ use teloxide::{
 };
 
 const USER_AGENT: &str =
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 
-    Safari/537.36 Edg/107.0.1418.24";
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24";
 
 const POPULAR_UP: &[u64] = &[431073645, 635041, 1773346];
 
